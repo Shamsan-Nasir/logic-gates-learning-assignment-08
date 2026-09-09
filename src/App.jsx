48a9575427e13d1cdb1a68cfa0854f87b3ff490a
Shamsan-Nasir <shamsannsr@gmail.com>
2026-09-09 17:36:41 +0600

    loader and not stat message added

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import './App.css'
 import './index.css'
 import { Navbar } from './Component/Navbar'
 import { Footer } from './Component/Footer'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import { TimeLineContext } from './Context'
 
 
 function App() {
-
+  let navigation = useNavigation()
   let [timelineCart, setTimelineCart] = useState([])
   return (
     <TimeLineContext.Provider value={{ timelineCart, setTimelineCart }} >
       <div className='max-w-400 mx-auto'>
         <Navbar></Navbar>
-        <Outlet></Outlet>
+        {
+          navigation.state === 'loading' ? <div className='flex justify-center items-center w-full p-5 h-1/2 bg-gray-300'> <span className="loading loading-spinner text-neutral w-25"></span></div>
+
+            :
+            <Outlet></Outlet>
+        }
+
         <Footer></Footer>
         <ToastContainer autoClose={1000}></ToastContainer>
       </div>
